719b7dc4e27d0e333917cd7f306b4f31f6da6b27
bkaki <bhaskarkakii77@gmail.com>
2023-05-05 03:41:56 +0530

    terraform

--- data.tf
+++ data.tf
@@ -6,5 +6,4 @@ data "aws_ami" "centos" {
 
 data "aws_security_group" "allow-all" {
   name = "allow-all"
-
 }
--- servers.tf
+++ servers.tf
@@ -1,9 +1,8 @@
-
 resource "aws_instance" "instance" {
    for_each               = var.components
    ami                    = "data.aws_ami.centos.image.id"
    instance_type          = each.value["instance_type"]
-   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
    tags = {
      Name = each.value["name"]
