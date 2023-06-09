59241a5233d56a90607ba9585bc0742fcce2404f
bkaki <bhaskarkakii77@gmail.com>
2023-06-09 12:03:50 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,21 +1,83 @@
-module "database-servers" {
-  for_each = var.database_servers
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
 
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
-  provisioner    = true
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+resource "aws_instance" "shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
 }
 
-module "app-servers" {
-  depends_on = [module.database-servers]
-  for_each   = var.app_servers
 
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
-}
+
