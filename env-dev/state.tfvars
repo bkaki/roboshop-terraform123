3ac24ad3b7a36d7887ca5e1b19380e4d07aa5b20
bkaki <bhaskarkakii77@gmail.com>
2023-08-19 16:53:39 +0530

    terraform

--- env-dev/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "terraform-b31"
-key    = "roboshop/dev/terraform.tfstate"
-region = "us-east-1"
--- module/vars.tf
+++
@@ -1,4 +0,0 @@
-variable "env" {}
-variable "component_name" {}
-variable "instance_type" {}
-variable "password" {}
