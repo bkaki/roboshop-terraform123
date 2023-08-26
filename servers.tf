29296cece92192cc03506b43cfb02ee019428858
bkaki <bhaskarkakii77@gmail.com>
2023-08-26 19:36:00 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -13,23 +13,70 @@
    default = "t3.micro"
  }
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  for_each = var.components
   ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
   tags = {
-    Name = "frontend"
+    Name = each.value["name"]
   }
 }
 
+ variable "components" {
+      default = {
+      frontend = {
+       name          = "frontend"
+       instance_type = "t3.micro"
+     }
+     catalogue = {
+       name          = "catalogue"
+       instance_type = "t3.micro"
+     }
+     mongodb = {
+       name          = "mongodb"
+       instance_type = "t3.micro"
+     }
+     cart = {
+       name          = "cart"
+       instance_type = "t3.micro"
+     }
+     redis = {
+       name          = "redis"
+       instance_type = "t3.micro"
+     }
+     user = {
+       name          = "user"
+       instance_type = "t3.micro"
+     }
+     rabbitmq = {
+       name          = "rabbitmq"
+       instance_type = "t3.micro"
+     }
+     mysql = {
+       name          = "mysql"
+       instance_type = "t3.micro"
+     }
+     shipping = {
+       name          = "shipping"
+       instance_type = "t3.micro"
+     }
+     payment = {
+       name          = "payent"
+       instance_type = "t3.micro"
+     }
+   }
+ }
 
-# resource "aws_route53_record" "frontend" {
+# resource "aws_route53_record" "records" {
 #   zone_id = "Z01900082F7LR70JYHJFY"
 #   name    = "frontend-dev.bhaskar77.online"
 #   type    = "A"
 #   ttl     = 30
 #   records = [aws_instance.frontend.private_ip]
 # }
+
+
 # resource "aws_instance" "catalogue" {
 #  ami           = data.aws_ami.centos.image_id
 #  instance_type = "t3.micro"
