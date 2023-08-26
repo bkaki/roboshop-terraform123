e8a1ca54a5d26fcd6a6edcbe7e8fd4230f6ac906
bkaki <bhaskarkakii77@gmail.com>
2023-08-26 17:03:25 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -6,22 +6,18 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 
-  output "aws_instance" {
+output "aws_instance" {
     value = aws_instance.frontend.public_ip
   }
 
-
-
-data "aws_ami" "centos" {
-    owners      = [ "973714476881" ]
+ data "aws_ami" "centos" {
+    owners      = ["973714476881"]
     most_recent = true
-    n ame_regex  = "Centos-8-DevOps-Practice"
-
-  }
-
-  output "ami" {
+    name_regex  = "Centos-8-DevOps-Practice"
+ }
+   output "ami" {
     value = data.aws_ami.centos.image_id
-  }
+   }
 
 
 
