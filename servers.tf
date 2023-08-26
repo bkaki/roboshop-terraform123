9388df39e94fe4cd0a161495e2ea63690ea8d095
bkaki <bhaskarkakii77@gmail.com>
2023-08-26 14:02:50 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -13,12 +13,12 @@ output "aws_instance" {
 
 
 data "aws_ami" "centos" {
-  owners           = [ "973714476881" ]
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 
 }
 
 output "ami" {
-  value = "data.aws_ami.centos.image_id"
+  value = data.aws_ami.centos.image_id
 }
