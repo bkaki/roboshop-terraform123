eca7d8b5533f9c7f9afd48840f1ac0afc27dd61a
bkaki <bhaskarkakii77@gmail.com>
2023-08-26 16:59:07 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -5,91 +5,91 @@ resource "aws_instance" "frontend" {
   tags = {
     Name = "frontend"
   }
-}
 
-output "aws_instance" {
-  value = aws_instance.frontend.public_ip
-}
+  output "aws_instance" {
+    value = aws_instance.frontend.public_ip
+  }
+
 
 
 data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+    owners      = [ "973714476881" ]
+    most_recent = true
+    n ame_regex  = "Centos-8-DevOps-Practice"
 
-}
-
-output "ami" {
-  value = data.aws_ami.centos.image_id
-}
-
-resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "frontend"
   }
 
-  resource "aws_instance" "frontend" {
-    ami           = "ami-03265a0778a880afb"
-    instance_type = "t3.micro"
-
-    tags = {
-      Name = "frontend"
-    }
-    resource "aws_instance" "frontend" {
-      ami           = "ami-03265a0778a880afb"
-      instance_type = "t3.micro"
-
-      tags = {
-        Name = "frontend"
-      }
-
-      resource "aws_instance" "frontend" {
-        ami           = "ami-03265a0778a880afb"
-        instance_type = "t3.micro"
-
-        tags = {
-          Name = "frontend"
-        }
-
-        ami           = "ami-03265a0778a880afb"
-        instance_type = "t3.micro"
-
-        tags = {
-          Name = "frontend"
-        }
+  output "ami" {
+    value = data.aws_ami.centos.image_id
+  }
 
 
-        resource "aws_instance" "frontend" {
-          ami           = "ami-03265a0778a880afb"
-          instance_type = "t3.micro"
 
-          tags = {
-            Name = "frontend"
-          }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#resource "aws_instance" "frontend" {
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "frontend"
+#  }
 
-          resource "aws_instance" "frontend" {
-            ami           = "ami-03265a0778a880afb"
-            instance_type = "t3.micro"
 
-            tags = {
-              Name = "frontend"
-            }
 
-            resource "aws_instance" "frontend" {
-              ami           = "ami-03265a0778a880afb"
-              instance_type = "t3.micro"
 
-              tags = {
-                Name = "frontend"
-              }
 
-              resource "aws_instance" "frontend" {
-                ami           = "ami-03265a0778a880afb"
-                instance_type = "t3.micro"
-
-                tags = {
-                  Name = "frontend"
-                }
