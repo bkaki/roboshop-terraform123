d12d1a535e5e39536f43904f761273d320c9d5ef
bkaki <bhaskarkakii77@gmail.com>
2023-09-12 18:42:16 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -6,25 +6,25 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
-}
-
 
   provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = self.private_ip
-  }
 
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
     inline = [
       "rm-rf roboshop-shell",
-     "git clone https://github.com/bkaki/roboshop-shell.git",
+      "git clone https://github.com/bkaki/roboshop-shell.git",
       "cd roboshop-shell",
       "sudo bash ${each.value["name"]}.sh"
     ]
-  }
+
+   }
 }
+
  resource "aws_route53_record" "records" {
    for_each = var.components
    zone_id = "Z01900082F7LR70JYHJFY"
